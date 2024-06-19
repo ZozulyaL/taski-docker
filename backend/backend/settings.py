3509c0726e4bbaef213323728904c5374feb1886
Людмила Зозуля <zozulyalud@yandex.ru>
2024-06-19 11:52:32 +0300

    add flake8

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,7 +1,6 @@
 import os
 from pathlib import Path
 
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -9,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['84.201.137.185','127.0.0.1','localhost', 'yapsati.zapto.org']
+ALLOWED_HOSTS = ['84.201.137.185', '127.0.0.1', 'localhost', 'yapsati.zapto.org']
 
 
 # Application definition
